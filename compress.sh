652fe0d7247fdd55337854ebc7682caf364ccea0
Bobbi Winema Yogatama <34973829+bwyogatama@users.noreply.github.com>
2024-05-03 18:42:50 -0500

    Update compress.sh

--- compress.sh
+++ compress.sh
@@ -1,6 +1,6 @@
 bin=./bin/ssb/binpack
 rbin=./bin/ssb/rlebinpack
-dbin=./bin/ssb/rlebinpack
+dbin=./bin/ssb/deltabinpack
 
 arr=("lo_custkey" "lo_partkey" "lo_suppkey" "lo_orderdate" "lo_quantity" "lo_extendedprice" "lo_discount" "lo_revenue" "lo_supplycost" "lo_orderkey" "lo_linenumber" "lo_tax" "lo_ordtotalprice" "lo_commitdate")
 for val in ${arr[*]}; do
